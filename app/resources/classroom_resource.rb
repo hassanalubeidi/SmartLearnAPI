661392467088212d07e6f4a631fca45c023c48e4
hassanalubeidi <hassanalubeidi1996@gmail.com>
2016-03-25 18:41:55 +0000

    added attributes to classroom resource

--- app/resources/classroom_resource.rb
+++ app/resources/classroom_resource.rb
@@ -1,5 +1,5 @@
 class ClassroomResource < JSONAPI::Resource
-  attributes :title, :users, :joined
+  attributes :title, :users, :year_group, :subject_id, :joined
 
   def joined
   	if @model.users.include? context[:current_user] then
