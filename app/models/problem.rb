ae7ce2c7d9fb663be920cb126a0816ab2e2a16d9
hassanalubeidi <hassanalubeidi1996@gmail.com>
2016-03-27 19:07:21 +0100

    added detail to problem resouces

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -8,6 +8,20 @@ class Problem < ActiveRecord::Base
 	validates :what_went_wrong, presence: true
 	validates :topic_id, presence: true
 
+	def test_paper
+		if self.answer == nil then
+			return nil 
+		else
+			return self.answer.question.main_question.test_paper
+		end
+	end
+
+	def detail 
+		unless self.test_paper == nil then
+			return "#{self.test_paper.title} #{self.answer.question.position} #{self.answer.marks} / #{self.answer.question.out_of}"
+		end
+	end
+
 	def timeago
 		return time_ago_in_words(self.created_at)
 	end
