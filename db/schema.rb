86df41d265e0201ea3730d2da8a4323b7cec8990
James Hartley <jhartley58@gmail.com>
2016-04-17 20:47:52 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320203624) do
+ActiveRecord::Schema.define(version: 20160417204445) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "marks",       limit: 4
@@ -88,6 +88,12 @@ ActiveRecord::Schema.define(version: 20160320203624) do
     t.text     "mark_scheme_html", limit: 65535
     t.integer  "out_of",           limit: 4
     t.string   "position",         limit: 255
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "status",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "subjects", force: :cascade do |t|
