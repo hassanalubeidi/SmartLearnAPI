8af07f610247150040ae81aa85f51b4cf009d3ea
hassanalubeidi <hassanalubeidi1996@gmail.com>
2016-05-11 09:48:45 +0100

    test_paper_details controller

---
+++ app/controllers/test_paper_details_controller.rb
@@ -0,0 +1,8 @@
+class TestPaperDetailsController < ApplicationController
+	before_action :authenticate_user!
+	include JSONAPI::ActsAsResourceController 
+
+	def context
+	    {current_user: current_user}
+	  end
+end
